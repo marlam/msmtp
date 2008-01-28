2dd18244104c955d309e446785f9b9526e8da7b3
Martin Lambers <marlam@marlam.de>
2008-01-28 05:44:31 +0000

    Print the recipients of a mail in msmtp-runqueue.sh. Patch by Konrad Schrempf. Thanks!

--- scripts/msmtpqueue/msmtp-runqueue.sh
+++ scripts/msmtpqueue/msmtp-runqueue.sh
@@ -34,7 +34,7 @@ touch "$LOCKFILE" || exit 1
 
 # process all mails
 for MAILFILE in *.mail; do
-	echo "*** Sending $MAILFILE..."
+	echo "*** Sending $MAILFILE to `sed -e 's/^.*-- \(.*$\)/\1/' $MSMTPFILE` ..."
 	MSMTPFILE="`echo $MAILFILE | sed -e 's/mail/msmtp/'`"
 	if [ ! -f "$MSMTPFILE" ]; then
 		echo "No corresponding file $MSMTPFILE found"

marlam/msmtp

commit 4d8c003657623777b91713b7b42a36c66a6b3902

Fix bugs in msmtp_read_addresses(), including a crash.
1. When --read-envelope-from is used but not -t/--read-recipients,
   msmtp can crash when trying to append addresses to a list that
   does not exist (the pointer is NULL).
2. When --read-envelope-from is used, it remembers only the first
   address. This is problematic if the address is split over two lines,
   in which case only the first line will be remembered, which is wrong
   since it usually only contains the user name. So now remember the
   last address instead.

Reported by Ilia Kudirov.

src/msmtp.c
@@ -1474,17 +1474,17 @@ int msmtp_read_addresses(FILE *mailf, FILE *tmpfile,
         {
             /* The current recipient just ended. Add it to the list */
             current_recipient[current_recipient_len] = '\0';
-            if (from_hdr == 0)
+            if (from_hdr >= 0)
             {
                 *from = current_recipient;
                 from_hdr = 1;
             }
-            else if (resent_block == 0)
+            else if (recipients && resent_block == 0)
             {
                 list_insert(resent_recipients, current_recipient);
                 resent_recipients = resent_recipients->next;
             }
-            else
+            else if (recipients)
             {
                 list_insert(normal_recipients, current_recipient);
                 normal_recipients = normal_recipients->next;

commit 8cce4b3a079dc718e8319f3c094deca5b5926795

Fix bug introduced in 4d8c003657623777b91713b7b42a36c66a6b3902.
This reverts the part of 4d8c003657623777b91713b7b42a36c66a6b3902 that tried
to fix address extraction from multi-line From headers. This broke the
combination of --read-envelope-from and -t badly: if From appears before To,
the recipients are not extracted.

Reported and analyzed by Nicolas Pouillard. See
http://article.gmane.org/gmane.mail.msmtp.user/349 for details.

This patch simply reverts to the old behaviour, which means only single-line
From headers are handled correctly by --read-envelope-from. This limitation is
now documented.

doc/msmtp.1
@@ -168,6 +168,8 @@ Resent-Cc, and Resent-Bcc headers in the first block of Resent- headers are
 used instead.
 .IP "\-\-read\-envelope\-from"
 Read the envelope from address from the From header of the mail.
+Currently this header must be on a single line for this option to work
+correctly.
 .IP "\-\-aliases=[\fIfile\fP]"
 Set or unset an aliases file. See the \fBaliases\fP command below.
 .IP "\-\-"

doc/msmtp.texi
@@ -662,6 +662,8 @@ used instead.
 @itemx --read-envelope-from
 @opindex --read-envelope-from
 Read the envelope from address from the From header of the mail.
+Currently this header must be on a single line for this option to work
+correctly.
 @itemx --aliases=[@var{file}]
 @opindex --aliases
 Set or unset an aliases file. @xref{aliases}.

src/msmtp.c
@@ -1474,7 +1474,7 @@ int msmtp_read_addresses(FILE *mailf, FILE *tmpfile,
         {
             /* The current recipient just ended. Add it to the list */
             current_recipient[current_recipient_len] = '\0';
-            if (from_hdr >= 0)
+            if (from_hdr == 0)
             {
                 *from = current_recipient;
                 from_hdr = 1;
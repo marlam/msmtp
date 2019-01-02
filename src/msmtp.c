fedc654accbe04f5bdf12c11b8680677cb98acd1
Martin Lambers <marlam@marlam.de>
2019-01-02 20:28:56 +0100

    Bump copyright year

--- src/msmtp.c
+++ src/msmtp.c
@@ -2585,7 +2585,7 @@ void msmtp_print_version(void)
     printf(_("User configuration file name: %s\n"), userconffile);
     free(userconffile);
     printf("\n");
-    printf(_("Copyright (C) 2018 Martin Lambers and others.\n"
+    printf(_("Copyright (C) 2019 Martin Lambers and others.\n"
                 "This is free software.  You may redistribute copies of "
                     "it under the terms of\n"
                 "the GNU General Public License "

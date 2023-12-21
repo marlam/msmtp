a6ed2e00323defc3e5aba11581fa4d3c0e4beeec
Tau <4602612+bash@users.noreply.github.com>
2023-12-21 17:43:32 +0100

    Comment constants

--- src/inet.h
+++ src/inet.h
@@ -1,6 +1,6 @@
 #ifndef INET_H
 #define INET_H
-#define INET_PROTOCOLS_IPV4  (1 << 0)
-#define INET_PROTOCOLS_IPV6  (1 << 1)
-#define INET_PROTOCOLS_ALL   (INET_PROTOCOLS_IPV4 | INET_PROTOCOLS_IPV6)
+#define INET_PROTOCOLS_IPV4  (1 << 0) /* Only use IPv4 address resolution. */
+#define INET_PROTOCOLS_IPV6  (1 << 1) /* Only use IPv6 address resolution. */
+#define INET_PROTOCOLS_ALL   (INET_PROTOCOLS_IPV4 | INET_PROTOCOLS_IPV6) /* Use both IPv4 and IPv6 address resolution. */
 #endif
